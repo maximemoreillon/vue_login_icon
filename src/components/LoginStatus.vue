755eef7747fa648eba60c37343a6eab6d896b655
Maxime Moreillon <m.moreillon@gmail.com>
2020-05-03 06:58:13 +0000

    simplification

--- src/components/LoginStatus.vue
+++ src/components/LoginStatus.vue
@@ -1,61 +1,50 @@
 <template>
-  <span
-    class="login_status"
-    v-on:mouseover="mouseover()"
-    v-on:mouseleave="mouseleave()">
+  <a
+    :href="authentication_front_url"
+    class="login_status">
 
+    <!-- transition from loading to not loading -->
+    <transition
+      name="fade"
+      mode="out-in">
 
-    <transition name="fade" mode="out-in">
-      <template v-if="!loading" >
+      <!-- if loading -->
+      <Loader
+        v-if="loading"
+        class="loader"/>
 
-        <!-- MAKE COMPONENT FROM HERE -->
+      <alert-circle-outline-icon
+        v-else-if="error"/>
+
+      <!-- If not loading or errored -->
+      <template v-else >
+
         <template v-if="user">
 
-          <template v-if="!hover">
-            <!-- display user avatar if available -->
-            <img
-              class="status_icon"
-              v-if="user.properties.avatar_url"
-              v-bind:src="user.properties.avatar_url" >
+          <!-- display user avatar if available -->
+          <img
+            class="avatar"
+            v-if="user.properties.avatar_url"
+            v-bind:src="user.properties.avatar_url" >
 
-            <!-- otherwise display generic icon -->
-            <account-icon
-              v-else
-              class="status_icon" />
-          </template>
-
-          <template v-else>
-
-            <logout-icon
-              class="control_icon"
-              v-on:click="logout()"/>
-          </template>
-
-        </template>
-
-
-        <!-- MAKE COMPONENT TO HERE -->
-
-        <!-- display "not logged in" icon -->
-        <template v-else>
-          <account-off-icon
-            v-if="!hover"
+          <!-- otherwise display generic icon -->
+          <account-icon
+            v-else
             class="status_icon" />
 
-          <login-icon
-            v-else
-            class="control_icon"
-            v-on:click="login()"/>
         </template>
 
+        <!-- display "not logged in" icon -->
+        <account-off-icon
+          v-else
+          class="status_icon" />
 
 
       </template>
 
-      <Loader v-if="loading"/>
     </transition>
 
-  </span>
+  </a>
 </template>
 
 <script>
@@ -64,10 +53,9 @@ import axios from 'axios'
 import VueCookies from 'vue-cookies'
 import Loader from '@moreillon/vue_loader'
 
-import AccountIcon from 'vue-material-design-icons/Account.vue';
-import AccountOffIcon from 'vue-material-design-icons/AccountOff.vue';
-import LoginIcon from 'vue-material-design-icons/Login.vue';
-import LogoutIcon from 'vue-material-design-icons/Logout.vue';
+import AccountIcon from 'vue-material-design-icons/Account.vue'
+import AccountOffIcon from 'vue-material-design-icons/AccountOff.vue'
+import AlertCircleOutlineIcon from 'vue-material-design-icons/AlertCircleOutline.vue'
 
 
 export default {
@@ -78,14 +66,16 @@ export default {
     // Icons
     AccountIcon,
     AccountOffIcon,
-    LoginIcon,
-    LogoutIcon,
+    AlertCircleOutlineIcon,
   },
   data(){
     return {
       user: null,
       loading: false,
       hover: false,
+      error: false,
+      authentication_api_url: 'https://api.authentication.maximemoreillon.com/whoami',
+      authentication_front_url: 'https://authentication.maximemoreillon.com/',
     }
   },
   mounted(){
@@ -98,66 +88,50 @@ export default {
 
       if(jwt) {
         this.loading = true;
-        axios.post('https://authentication.maximemoreillon.com/whoami', {},
+        axios.post(`${this.authentication_api_url}`, {},
         { headers: { Authorization : `Bearer ${jwt}`} })
-        .then(response => { this.user = response.data})
-        .catch( () => { /* Nothing */ })
+        .then(response => {
+          this.user = response.data
+        })
+        .catch( () => {
+          this.error = true
+        })
         .finally( () => {
           this.loading = false
           this.$emit('loadingFinished')
 
         })
-
       }
     },
-    mouseover(){
-      this.hover = true;
-    },
-    mouseleave(){
-      this.hover = false;
-    },
-    logout(){
-      VueCookies.remove('jwt')
-      // Not very elegant way to deal with this
-      location.reload()
-    },
-    login(){
-      location.href = "https://authentication.maximemoreillon.com/"
-    },
+
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style>
+<style scoped>
 /* Not scoped because need to affect icons */
 .login_status{
   display: inline-flex;
   align-items: center;
-
   cursor: pointer;
+  text-decoration: none;
+  color: currentColor;
 }
 
 
-.login_status img {
+.avatar, .material-design-icon__svg {
   height: 1em;
   width: 1em;
 }
 
-.login_status .material-design-icon {
-  height: 1em;
-  width: 1em;
-}
 
-.material-design-icon > .material-design-icon__svg {
-  height: 1em;
-  width: 1em;
-}
+
 
 .fade-enter-active, .fade-leave-active {
   transition: opacity .25s;
 }
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+.fade-enter, .fade-leave-to {
   opacity: 0;
 }
 
